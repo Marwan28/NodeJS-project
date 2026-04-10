5c80c17e0765f1d16a6a30415b95c9d9ab14365b
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-04-11 00:13:05 +0200

    edit routers

--- features/promo_codes/promo_codes_router.js
+++ features/promo_codes/promo_codes_router.js
@@ -6,12 +6,13 @@ import {
   updatePromoCode,
   deletePromoCode,
 } from "./promo_code_controller.js";
+import { verifyAdmin, verifyToken } from "../users/user_middleware.js";
 
 const promoCodeRouter = express.Router();
 promoCodeRouter.get("/promo-codes", readPromoCodes);
 promoCodeRouter.get("/promo-codes/:id", readPromoCodeById);
-promoCodeRouter.post("/promo-codes", createPromoCode);
-promoCodeRouter.put("/promo-codes/:id", updatePromoCode);
-promoCodeRouter.delete("/promo-codes/:id", deletePromoCode);
+promoCodeRouter.post("/promo-codes",verifyToken ,verifyAdmin, createPromoCode);
+promoCodeRouter.put("/promo-codes/:id", verifyToken, verifyAdmin, updatePromoCode);
+promoCodeRouter.delete("/promo-codes/:id", verifyToken, verifyAdmin, deletePromoCode);
 
 export default promoCodeRouter;
